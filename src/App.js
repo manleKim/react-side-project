4231615647450555b471affd6202fc0f271ee67a
manleKim <http://github.com/manleKim>
2023-07-13 17:10:22 +0900

    Dependencies

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [value, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
   useEffect(() => {
-    console.log("CALL THE API....");
+    console.log("I run only once");
   }, []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'value' changes");
+  }, [value]);
+  useEffect(() => {
+    console.log("I run when keyword & value change");
+  }, [keyword, value]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{value}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
